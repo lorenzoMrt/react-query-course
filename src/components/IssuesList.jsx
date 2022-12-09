02e49e9757d7bf523f06fc9ba9f7ee4a38d57859
lorenzo miret <lmiret@biobam.com>
2022-12-09 09:27:21 +0100

    Issues search

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useQuery } from "react-query";
 import { IssueItem } from "./IssueItem";
 
@@ -9,12 +10,44 @@ export default function IssuesList({ labels, status }) {
       res.json()
     );
   });
+
+  const [searchValue, setSearchValue] = useState("");
+
+  const searchQuery = useQuery(
+    ["issues", "search", searchValue],
+    () =>
+      fetch(`/api/search/issues?q=${searchValue}`).then((res) => res.json()),
+    {
+      enabled: searchValue.length > 0,
+    }
+  );
+
   return (
     <div>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setSearchValue(event.target.elements.search.value);
+        }}
+      >
+        <label htmlFor="search"> Search Issues</label>
+        <input
+          type="search"
+          placeholder="search"
+          name="search"
+          id="search"
+          onChange={(ev) => {
+            if (ev.target.value.length === 0) {
+              setSearchValue("");
+            }
+          }}
+        ></input>
+      </form>
       <h2>Issues List</h2>
       {isLoading ? (
         <p>Loading...</p>
-      ) : (
+      ) : searchQuery.fetchStatus === "idle" &&
+        searchQuery.isLoading === true ? (
         <ul className="issues-list">
           {data.map((issue) => (
             <IssueItem
@@ -30,6 +63,32 @@ export default function IssuesList({ labels, status }) {
             />
           ))}
         </ul>
+      ) : (
+        <>
+          <h2>Search results</h2>
+          {searchQuery.isLoading ? (
+            <p>Loading...</p>
+          ) : (
+            <>
+              <p>{searchQuery.data.count} Results</p>
+              <ul className="issues-list">
+                {searchQuery.data.items.map((issue) => (
+                  <IssueItem
+                    key={issue.id}
+                    title={issue.title}
+                    number={issue.number}
+                    assignee={issue.assignee}
+                    commentCount={issue.comments.length}
+                    createdBy={issue.createdBy}
+                    createdDate={issue.createdDate}
+                    labels={issue.labels}
+                    issue={issue.status}
+                  />
+                ))}
+              </ul>
+            </>
+          )}
+        </>
       )}
     </div>
   );
