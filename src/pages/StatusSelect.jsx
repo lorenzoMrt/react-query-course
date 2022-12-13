563f6dea5b586c68f625d52b078d2fefb106d5da
lmiret <16326209+lorenzoMrt@users.noreply.github.com>
2022-12-13 11:03:01 +0100

    FEAT: Update status

--- src/pages/StatusSelect.jsx
+++ src/pages/StatusSelect.jsx
@@ -5,10 +5,10 @@ const statusList = [
   { id: "done", label: "Done" },
   { id: "cancelled", label: "Cancelled" },
 ];
-export function StatusSelect({ value, onChange }) {
+export function StatusSelect({ value, onChange, noEmptyOption = false }) {
   return (
     <select value={value} onChange={onChange} className={"status-select"}>
-      <option value={""}>Select status</option>
+      {noEmptyOption ? null : <option value={""}>Select status</option>}
       {statusList.map((status) => (
         <option value={status.id} key={status.id}>
           {status.label}
